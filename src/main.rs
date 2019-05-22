10158ac6b67fdacf046d37e30cd4f26427074678
a3r <arrrght@gmail.com>
2019-05-22 15:29:41 +0500

    delete unnecessary vars

--- src/main.rs
+++ src/main.rs
@@ -2,60 +2,52 @@ use futures::future;
 use hyper::rt::{Future, Stream};
 use hyper::service::service_fn;
 use hyper::{Method, Body, Client, Request, Response, Server, StatusCode};
-use std::io::{self, Write};
 use std::collections::HashMap;
 use rand::Rng;
 use std::time::Instant;
 use std::sync::{Arc, Mutex};
 use bytes::{ Buf, Bytes, IntoBuf };
-use url::{Url, ParseError};
 
 struct Prox {
     url: String,
     date: std::time::Instant
 }
-impl Prox {
-    fn new(url: String) -> Prox {
-        Prox { url: url, date: std::time::Instant::now() }
-    }
-}
 
-fn change_req(label: usize, mut r: Arc<Mutex<HashMap<usize, String>>>, mut req: Request<Body>) -> Request<Body>{
-    println!("REQ1 {:?}", req);
+fn get_random_proxy() -> String {
     let mut rng = rand::thread_rng();
     let urls = ["http://cap.avtocod.ru", "http://cap2.avtocod.ru"];
-    let rnd_url = urls[rng.gen_range(0,urls.len())].to_string();
+    urls[rng.gen_range(0,urls.len())].to_string()
+}
 
+fn change_req(proxy_now: String, mut r: Arc<Mutex<HashMap<usize, String>>>, mut req: Request<Body>) -> Request<Body>{
+    println!("REQ1 {:?}", req);
     println!("REQM {:?}", req.method());
 
-    r.lock().expect("here1");
     let mut lock = match r.lock() {
         Ok(guard) => guard,
         Err(poison) => poison.into_inner()
     };
-    let now_proxy = if req.method() == Method::GET {
+
+    let proxy_insert = if req.method() == Method::GET {
         let s1: String = req.uri().query().unwrap().to_string().split("&").filter(|x| &x[0..3] == "id=").collect();
         let s2 = match &s1[3..].parse::<usize>() {
             Ok(r) => r.to_owned(),
             Err(_e) => 0 as usize
         };
-        let url4get = lock.get(&s2).unwrap().to_owned();
-        println!("GOT GET {}, {:?} {:?}", s2, label, url4get);
+        println!("TRY GET {:?} {:?} -- {:?}", s2, req, lock);
+        let url4get = lock.get(&s2).expect("here1").to_owned();
+        println!("GOT GET {}, {:?}", s2, url4get);
         url4get
     }else{ 
-        match lock.contains_key(&label) {
-            true => lock.get(&label).unwrap().to_owned(),
-            false => { lock.insert(label, rnd_url.to_string()); rnd_url }
-        }
+        proxy_now
     };
 
-    println!("GOT THIS: {}:: {:?}", now_proxy, lock);
+    println!("GOT THIS: {}:: {:?}", proxy_insert, lock);
 
-    let uri_string = format!("{}{}", now_proxy, req.uri().path_and_query().map(|x| x.as_str()).unwrap_or(""));
+    let uri_string = format!("{}{}", proxy_insert, req.uri().path_and_query().map(|x| x.as_str()).unwrap_or(""));
     let uri = uri_string.parse().expect("here2");
     *req.uri_mut() = uri;
     req.headers_mut().remove("host");
-    //req.headers_mut().insert("label", label.to_string().parse().unwrap());
     req
 }
 
@@ -74,16 +66,15 @@ fn main() {
         let inner = Arc::clone(&r);
 
         service_fn(move |req| {
-            let mut rng = rand::thread_rng();
-            let path_var: usize = rng.gen();
+            let proxy_now = get_random_proxy();
+            let proxy_now2 = proxy_now.clone();
             let inner2 = Arc::clone(&inner);
-            let req = change_req(path_var, inner2, req);
             let inner3 = Arc::clone(&inner);
-            println!("PATH_REQ: {}", path_var);
+            let req = change_req(proxy_now, inner2, req);
+            println!("REDIR_REQ: {} / {}", req.method(), req.uri());
             client
                 .request(req)
                 .and_then(move |res| {
-                    println!("PATH_RES: {}", path_var);
                     println!("res: {:?}", res);
                     res.into_body().concat2()
                 })
@@ -106,16 +97,8 @@ fn main() {
                                     Ok(guard) => guard,
                                     Err(poison) => poison.into_inner()
                                 };
-                                match lock.contains_key(&path_var) {
-                                    true => {
-                                        let proxy = lock.get(&path_var).unwrap().to_owned();
-                                        lock.insert(ok_answer, proxy);
-                                    },
-                                    false => () //panic!("panicked as a bitch")
-                                };
+                                lock.insert(ok_answer, proxy_now2);
                             }
-
-
                             future::ok(Response::new(Body::from(body)))
                         },
                         Err(_e) => {
